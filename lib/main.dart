a1999b3d24ccc62699e1c086be91fce0e112fd46
Abdul Wahid Kahar <76991016+Abdwhidd@users.noreply.github.com>
2022-05-10 15:44:57 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:dicoding_news_app/pages/news_list_page.dart';
+import 'package:flutter/material.dart';
+
+import 'article.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => NewsListPage(),
+      },
+    );
+  }
+}
+
+
